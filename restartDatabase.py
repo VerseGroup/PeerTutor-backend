cc64927634e5fe17a87ad7f2c0879130a06bea05
Paul Evans <pauljevans927@gmail.com>
2021-10-09 22:00:54 -0400

    clarified admin page in relation to new resource

--- restartDatabase.py
+++ restartDatabase.py
@@ -1,3 +1,5 @@
+#THIS FILE IS SOLEY FOR DEVELOPMENT PURPOSES AND SHOULD BE DELETED BEFORE DEPLOYMENT
+
 from PeerTutor import db
 db.drop_all()
 from PeerTutor.models import User, Course, CourseRequest, Match, Schedule
