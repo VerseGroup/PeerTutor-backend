63e6c540cf4660e7f8b17679bc7eeeca7cf1bdbd
Paul Evans <pauljevans927@gmail.com>
2021-10-14 23:30:39 -0400

    finished start of match function (except period)

--- PeerTutor/algorithims/match.py
+++ PeerTutor/algorithims/match.py
@@ -10,15 +10,22 @@ def matchRequests(request):
     def makeMatch(request1, request2):
         db.session.delete(request1)
         db.session.delete(request2)
-        try: 
-            db.session.commit()
-
-            
-
-            match = Match()
-        except:
-            None
+        db.session.commit()
         
+        if request1.relationship == True:
+            tutor_id = request1.user_id
+            tutee_id = request2.user_id
+        else:
+            tutor_id = request2.user_id
+            tutee_id = request1.user_id
+
+        #ADD PERIOD CALCULATOR
+        period = "example"
+        
+        match = Match(tutor_id=tutor_id, tutee_id=tutee_id, course_id=request1.course_id, period=period)
+        db.session.add(match)
+        db.session.commit()
+            
     match = checkMatch(request)
     if match is not None:
         return makeMatch(request, match)
