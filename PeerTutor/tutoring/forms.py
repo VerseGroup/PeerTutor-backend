5fe38de06b2ff746b396f3c3f68284a434ea9791
Paul Evans <pauljevans927@gmail.com>
2021-10-20 19:00:58 -0400

    Added requests (all related funcs/templates)

---
+++ PeerTutor/tutoring/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
+from PeerTutor.models import User
+
+COURSE_CHOICES = [('1', '8am'), ('2', '10am')] #Generate list of choices
+TEACHING_CHOICES = [('1', 'Teach'), ('2', 'Learn')]
+
+class RequestMatch(FlaskForm):
+    course = SelectField('Course', choices=COURSE_CHOICES,
+        validators=[DataRequired()])
+    relationship = SelectField('Teach or Learn?', choices=TEACHING_CHOICES,
+        validators=[DataRequired()])
+    submit = SubmitField('Request')
--- PeerTutor/tutoring/routes.py
+++ PeerTutor/tutoring/routes.py
@@ -1,5 +1,36 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, redirect, url_for, flash
+from flask_login.utils import login_required
+from PeerTutor import db
 from PeerTutor.models import User, Match, CourseRequest, Course, Schedule
+from flask_login import current_user, login_required
+from PeerTutor.tutoring.forms import RequestMatch, COURSE_CHOICES, TEACHING_CHOICES
 
 tutor_functions = Blueprint('tutor_functions', __name__)
 
+@login_required
+@tutor_functions.route('/requests', methods=['GET'])
+def requests():
+    return render_template('requests.html')
+    
+@login_required
+@tutor_functions.route('/request', methods=['GET', 'POST'])
+def makeRequest():
+    form = RequestMatch()
+    if form.validate_on_submit():
+
+        course_id = dict(COURSE_CHOICES).get(form.course.data)
+        relationship = dict(TEACHING_CHOICES).get(form.relationship.data)
+
+        courseR = CourseRequest(username=current_user, course_id=course_id, 
+            relationship=relationship)
+        
+        db.session.add(courseR)
+        db.session.commit()
+        flash('Sent Request!', 'success')
+        return redirect(url_for('tutoring.requests'))
+    return render_template('/request.html', form=form)
+
+@login_required
+@tutor_functions.route('/sessions', methods=['GET'])
+def sessions():
+    pass
