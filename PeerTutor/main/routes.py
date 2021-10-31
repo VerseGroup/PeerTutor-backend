aebfee72343dd0082cad9f8a7257fa34d661b6f8
malcolm <malcolm.krolick@gmail.com>
2021-10-31 18:47:59 -0400

    About.html and route change

--- PeerTutor/main/routes.py
+++ PeerTutor/main/routes.py
@@ -4,9 +4,13 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 @main.route("/home")
-def home():    
+def home():
     return render_template('/home.html')
 
 @main.route("/apihelp")
 def apiHelp():
     return render_template('/api.html')
+
+@main.route("/about")
+def about():
+    return render_template('/about.html')
